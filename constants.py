1a5b91b5bebaf74127bc658f3bb8bae47b8f8da7
Omari Matthews <omatthews@fb.com>
2022-08-29 13:26:02 -0400

    implemented secrets.py

--- constants.py
+++ constants.py
@@ -1,9 +1,10 @@
+import secrets
+
 USER_ID = 'a7e29731-49ab-4a26-956f-ca3ba4cfb580'
-API_TOKEN = '4ecace19-e29d-46a0-a890-1b082b78e8bd'
 CLIENT_HEADER = USER_ID + 'Testing'
 
 auth_dict = {
         "x-api-user": USER_ID,
-        "x-api-key": API_TOKEN,
+        "x-api-key": secrets.API_TOKEN,
         "x-client": CLIENT_HEADER
 }
